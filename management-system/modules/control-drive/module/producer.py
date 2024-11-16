50862336e74530dbc83c8edd85bd902e5baf6091
Ilshat <110959745+Ftr4vis@users.noreply.github.com>
2024-11-16 23:21:12 +0300

    Update producer.py

--- management-system/modules/control-drive/module/producer.py
+++ management-system/modules/control-drive/module/producer.py
@@ -10,9 +10,9 @@ _requests_queue: multiprocessing.Queue = None
 MODULE_NAME = os.getenv('MODULE_NAME')
 
 
-def proceed_to_deliver(id, details):
-    details['source'] = MODULE_NAME
-    _requests_queue.put(details)
+def proceed_to_deliver(event_details):
+    event_details['source'] = MODULE_NAME
+    _requests_queue.put(event_details)
 
 
 def producer_job(_, config, requests_queue: multiprocessing.Queue):
@@ -28,7 +28,7 @@ def producer_job(_, config, requests_queue: multiprocessing.Queue):
         producer.produce(
             topic,
             json.dumps(event_details),
-            event_details['id'],
+            event_details['event_id'],
             callback=delivery_callback
         )
 
